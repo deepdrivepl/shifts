c7a1283595c90d772228c89c57c7086eaafb9f56
marta-0 <kaczmarskamarta2@gmail.com>
2023-03-06 18:11:55 +0100

    PyTorch metrics; nDSC loss

--- mswml/metrics.py
+++ mswml/metrics.py
@@ -8,6 +8,10 @@ from scipy import ndimage
 from collections import Counter
 from joblib import Parallel, delayed
 from sklearn import metrics
+import torch
+from torch.nn.modules.loss import _Loss
+import torch.nn.functional as F
+
 
 
 def dice_metric(ground_truth, predictions):
@@ -31,6 +35,20 @@ def dice_metric(ground_truth, predictions):
         dice = 1.0
     else:
         dice = (2. * intersection) / union
+
+    return dice
+
+
+def dice_metric_pytorch(ground_truth, predictions):
+    # Calculate intersection and union of y_true and y_predict
+    intersection = torch.sum(predictions * ground_truth)
+    union = torch.sum(predictions) + torch.sum(ground_truth)
+
+    # Calcualte dice metric
+    if intersection == 0.0 and union == 0.0:
+        dice = 1.0
+    else:
+        dice = ((2. * intersection) / union).cpu()
 
     return dice
 
@@ -72,6 +90,38 @@ def dice_norm_metric(ground_truth, predictions):
         fp_scaled = k * fp
         dsc_norm = 2. * tp / (fp_scaled + 2. * tp + fn)
         return dsc_norm
+
+
+def get_tp_fn_fp(ground_truth, predictions):
+    tp = ((ground_truth == predictions) & (ground_truth == 1))
+    fn = ((ground_truth != predictions) & (ground_truth == 1))
+    fp = ((ground_truth != predictions) & (ground_truth == 0))
+
+    return tp, fn, fp
+
+
+def dice_norm_metric_pytorch(ground_truth, predictions):
+    # Reference for normalized DSC
+    r = 0.001
+
+    im_sum = torch.sum(predictions) + torch.sum(ground_truth)
+    if im_sum == 0:
+        return 1.0
+    else:
+        if torch.sum(ground_truth) == 0:
+            k = 1.0
+            tp = 0
+            fn = 0
+            fp = ((ground_truth != predictions) & (ground_truth == 0)).sum()
+
+        else:
+            k = (1 - r) * torch.sum(ground_truth) / (r * (torch.numel(ground_truth) - torch.sum(ground_truth)))
+            tp, fn, fp = get_tp_fn_fp(ground_truth, predictions)
+            tp, fn, fp = tp.sum(), fn.sum(), fp.sum()
+
+        fp_scaled = k * fp
+        dsc_norm = 2. * tp / (fp_scaled + 2. * tp + fn)
+        return dsc_norm.cpu()
 
 
 def ndsc_aac_metric(ground_truth, predictions, uncertainties, parallel_backend=None):
@@ -164,6 +214,24 @@ def ndsc_retention_curve(ground_truth, predictions, uncertainties, fracs_retaine
     return dsc_norm_scores
 
 
+def ndsc_retention_curve_pytorch(ground_truth, predictions, uncertainties, fracs_retained, device):
+    def compute_dice_norm(frac_, preds_, gts_, N_):
+        pos = int(N_ * frac_)
+        curr_preds = preds if pos == N_ else torch.concatenate(
+            (preds_[:pos], gts_[pos:]))
+        return dice_norm_metric_pytorch(gts_, curr_preds)
+
+    ordering = uncertainties.argsort()
+    gts = ground_truth[ordering].clone()
+    preds = predictions[ordering].clone()
+    N = len(gts)
+
+    dsc_norm_scores = torch.tensor([compute_dice_norm(preds_=preds, gts_=gts, N_=N, frac_=frac) for frac in fracs_retained],
+                                   device=device)
+
+    return dsc_norm_scores.cpu()
+
+
 def intersection_over_union(mask1, mask2):
     """
     Compute IoU for 2 binary masks.
@@ -175,6 +243,13 @@ def intersection_over_union(mask1, mask2):
       Intersection over union between `mask1` and `mask2` (`float` in [0.0, 1.0]).
     """
     return np.sum(mask1 * mask2) / np.sum(mask1 + mask2 - mask1 * mask2)
+
+
+def intersection_over_union_pytorch(mask1, mask2):
+    if torch.sum(mask1) + torch.sum(mask2) == 0:
+        return 1.0
+        
+    return (torch.sum(mask1 * mask2) / torch.sum(mask1 + mask2 - mask1 * mask2)).cpu()
 
 
 def lesion_f1_score(ground_truth, predictions, IoU_threshold=0.25, parallel_backend=None):
@@ -250,3 +325,39 @@ def lesion_f1_score(ground_truth, predictions, IoU_threshold=0.25, parallel_back
     f1 = 1.0 if tp + 0.5 * (fp + fn) == 0.0 else tp / (tp + 0.5 * (fp + fn))
 
     return f1
+
+
+class nDSC_Loss(_Loss):
+    # Ref.: https://www.kaggle.com/c/planet-understanding-the-amazon-from-space/discussion/35899
+    
+    def __init__(self, r=0.001):
+        super().__init__(reduction='mean')
+        self.r = r
+
+    def forward(self, predictions, ground_truth):
+        
+        ground_truth = torch.squeeze(ground_truth).to(predictions.dtype)
+
+        predictions = torch.squeeze(predictions[:,1])
+
+        im_sum = torch.sum(predictions) + torch.sum(ground_truth)
+
+        if im_sum == 0:
+            return 1.0 - 1.0
+    
+        else:
+            if torch.sum(ground_truth) == 0:
+                k = 1.0
+                tp_loss = 0
+                fn_loss = 0
+
+            else:
+                k = (1 - self.r) * torch.sum(ground_truth) / (self.r * (torch.numel(ground_truth) - torch.sum(ground_truth)))
+                tp_loss = (ground_truth * (1 - F.binary_cross_entropy_with_logits(predictions, ground_truth))).sum()
+                fn_loss = (ground_truth * F.binary_cross_entropy_with_logits(predictions, ground_truth)).sum()
+                
+            fp_loss = ((1 - ground_truth) * F.binary_cross_entropy_with_logits(predictions, ground_truth)).sum()
+
+            fp_scaled = k * fp_loss
+            dsc_norm = 2. * tp_loss / (fp_scaled + 2. * tp_loss + fn_loss)
+            return 1 - dsc_norm
