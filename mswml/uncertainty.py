f9be55a86528ab7ba8840f33c09f9155491a1102
marta-0 <kaczmarskamarta2@gmail.com>
2023-03-06 18:13:53 +0100

    PyTorch metrics

--- mswml/uncertainty.py
+++ mswml/uncertainty.py
@@ -3,6 +3,7 @@ Implementation of standard predictive uncertainty measures for image segmentatio
 """
 
 import numpy as np
+import torch
 
 def renyi_entropy_of_expected(probs, alpha=0.8):
     """
@@ -56,6 +57,44 @@ def ensemble_uncertainties_classification(probs, epsilon=1e-10):
     mutual_info = eoe - exe
 
     epkl = -np.sum(mean_probs * mean_lprobs, axis=-1) - exe
+
+    uncertainty = {'confidence': -1 * conf,
+                   'entropy_of_expected': eoe,
+                   'expected_entropy': exe,
+                   'mutual_information': mutual_info,
+                   'epkl': epkl,
+                   'reverse_mutual_information': epkl - mutual_info,
+                   }
+
+    return uncertainty
+
+
+def entropy_of_expected_pytorch(probs, epsilon=1e-10):
+    mean_probs = torch.mean(probs, axis=0)
+    log_probs = -torch.log(mean_probs + epsilon)
+    return torch.sum(mean_probs * log_probs, axis=-1)
+
+
+def expected_entropy_pytorch(probs, epsilon=1e-10):
+    log_probs = -torch.log(probs + epsilon)
+    return torch.mean(torch.sum(probs * log_probs, axis=-1), axis=0)
+
+
+def ensemble_uncertainties_classification_pytorch(probs, epsilon=1e-10):
+    """
+    :param probs: array [num_models, num_voxels_X, num_voxels_Y, num_voxels_Z, num_classes]
+    :return: Dictionary of uncertainties
+    """
+    mean_probs = torch.mean(probs, axis=0)
+    mean_lprobs = torch.mean(torch.log(probs + epsilon), axis=0)
+    conf = torch.max(mean_probs, axis=-1)
+
+    eoe = entropy_of_expected_pytorch(probs, epsilon)
+    exe = expected_entropy_pytorch(probs, epsilon)
+
+    mutual_info = eoe - exe
+
+    epkl = -torch.sum(mean_probs * mean_lprobs, axis=-1) - exe
 
     uncertainty = {'confidence': -1 * conf,
                    'entropy_of_expected': eoe,
